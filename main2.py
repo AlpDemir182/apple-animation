0618b39f228faf29cfb407bf4d1c16fbb7ae8d74
AlpDemir182 <hagunenjoyer7@gmail.com>
2025-02-16 12:54:56 +0100

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,40 @@
+import pygame
+import pgzrun
+import time
+
+q = int(input("how many cycles: "))
+
+WIDTH = 250
+HEIGHT = 250
+
+apple = Actor("apple")
+count = 0
+apple.x = 20
+apple.y = 125
+
+def draw():
+    global count
+    screen.clear()
+    apple.draw()
+    screen.draw.text("count: " +str(count), (20,10), color = "white", fontsize = 15)
+
+
+
+
+def startanimation():
+    global count, q
+    if count < q:
+        animate(apple, pos = (230,125), duration = 2, on_finished = endanimation)
+        count += 1
+    
+
+
+
+def endanimation():
+    animate(apple, pos = (10,125), duration = 2, on_finished = startanimation)
+
+#def moveforward():
+    #animate(apple, pos = (230,125), duration = 2, on_finished = endanimation)
+
+startanimation()
+pgzrun.go()
